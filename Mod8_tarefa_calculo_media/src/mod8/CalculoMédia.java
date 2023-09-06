241c6e9cf3967cc15def5e791ee289e9883ff3b4
Vinicius Ribeiro <vinicius.basto.r@gmail.com>
2023-09-06 20:04:18 -0300

    Exercicio mod8 calculo média

---
+++ Mod8_tarefa_calculo_media/src/mod8/CalculoMédia.java
@@ -0,0 +1,23 @@
+package mod8;
+
+public class CalculoMédia {
+		
+	public static void main(String[] args) {
+		double nota1 = 10.00;
+		double nota2 = 9.60;
+		double nota3 = 5.60;
+		double nota4 = 8.50;
+		double media = (nota1+nota2+nota3+nota4)/4;
+		
+		System.out.println("***** Calculo média *****\n");
+		
+		System.out.println("Nota 1: " + nota1 + ".");
+		System.out.println("Nota 2: " + nota2 + ".");
+		System.out.println("Nota 3: " + nota3 + ".");
+		System.out.println("Nota 4: " + nota4 + ".");
+		
+		System.out.println("\nA média é: " + media + ".");
+
+	}
+
+}
